ca9a16cb350e1129eae736e7c111913892f19507
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-18 20:06:33 +0530

    register page

--- frontend/src/pages/ListingDetails.jsx
+++ frontend/src/pages/ListingDetails.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import BookingForm from "../components/traveller/BookingForm";
 
-const ListingDetails = () => {
+export default function ListingDetails() {
+  const listingId = 12;
+
   return (
-    <div>ListingDetails</div>
-  )
-}
+    <div className="grid grid-cols-3 gap-8">
+      <div className="col-span-2">
+        {/* Listing info */}
+      </div>
 
-export default ListingDetails
+      <BookingForm listingId={listingId} />
+    </div>
+  );
+}
