f3f2e53cd2a5153857197ef5c6f8967865a7ecda
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-19 21:21:55 +0530

    backend fix

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,6 +7,5 @@ class User(models.Model):
     mobile = models.CharField(max_length=15)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return self.email
