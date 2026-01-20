0303f228faaed55e424f507086a9b9f893c19cc0
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-20 13:22:59 +0530

    backend fix

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from .views import RegisterView
+
 urlpatterns = [
-    path('',views.users_dashboard),
+    path('register/', RegisterView.as_view()),
 ]
