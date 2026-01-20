f67d702fafe506ff28431bace65ae57eaa28832e
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-20 13:21:59 +0530

    backend fix

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["name", "email", "mobile", "password"]
+        fields = ['name', 'email', 'mobile', 'password']
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
 
     def create(self, validated_data):
-        validated_data["password"] = make_password(validated_data["password"])
+        validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
