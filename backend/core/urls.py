e75235c8ad4a68c196b4733c78b28f171b3fad98
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-20 13:30:45 +0530

    register

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/', include('users.urls')),
     path('providers', include('providers.urls')),
     path('admins', include('admins.urls')),
---
+++ backend/users/utils.py
@@ -0,0 +1,12 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken()
+
+    refresh["user_id"] = user.id
+    refresh["email"] = user.email
+
+    return {
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+    }
