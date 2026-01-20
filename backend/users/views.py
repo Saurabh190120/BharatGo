a45c48b25f8438ce99a87cf6318932d355201185
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-20 13:22:32 +0530

    backend fix

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
-# Create your views here.
-def users_dashboard(request):
-    return HttpResponse("Welcome to the Users Dashboard")
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User registered successfully"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
