9a95e8929112831d5ecdc8e99c78a72109ff7dba
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-27 18:22:10 +0530

    user fix

--- backend/bookings/views.py
+++ backend/bookings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Booking
+from .serializers import BookingSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def create_booking(request):
+    serializer = BookingSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(["GET"])
+def my_bookings(request):
+    bookings = Booking.objects.filter(user=request.user)
+    serializer = BookingSerializer(bookings, many=True)
+    return Response(serializer.data)
