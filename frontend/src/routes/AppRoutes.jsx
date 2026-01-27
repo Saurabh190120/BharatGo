9d10029f53d197c085df091df7b14937f3e9e0ff
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-27 18:28:06 +0530

    user fix

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,27 +1,21 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import SearchResults from "../pages/SearchResults";
-import ListingDetails from "../pages/ListingDetails";
-import Booking from "../pages/Booking";
-import MyBookings from "../pages/MyBookings";
-import ProviderDashboard from "../pages/ProviderDashboard";
-import AdminDashboard from "../pages/AdminDashboard";
-import VerifyOTP from "../pages/VerifyOTP";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../components/traveller/pages/Home'
+import Login from '../auth/Login'
+import Register from '../auth/Register'
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/search" element={<SearchResults />} />
+      {/* <Route path="/search" element={<SearchResults />} />
       <Route path="/listing/:id" element={<ListingDetails />} />
       <Route path="/booking/:id" element={<Booking />} />
       <Route path="/my-bookings" element={<MyBookings />} />
       <Route path="/provider" element={<ProviderDashboard />} />
       <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/verify-otp" element={<VerifyOTP />} />
+      <Route path="/verify-otp" element={<VerifyOTP />} /> */}
     </Routes>
   );
 }
