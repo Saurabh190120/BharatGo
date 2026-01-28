5d00a1099b5110e913cb9c6a93ea7dca0f258291
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-28 16:50:17 +0530

    provider fix

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from users.models import User
 from listings.models import Listing
+from users.models import User
 
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
-    status = models.CharField(max_length=20, default="Confirmed")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, default="CONFIRMED")
