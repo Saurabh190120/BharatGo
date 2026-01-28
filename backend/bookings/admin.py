6509b325fb6a02d6f36d7614d5c3e633920e2394
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-28 17:07:47 +0530

    provider fix

--- backend/bookings/admin.py
+++ backend/bookings/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
-# from . models import Booking
-# # Register your models here.
-# admin.site.register(Booking)  # Replace ... with your model names to register them in the admin site.
+from django.contrib import admin
+from . models import Booking
+# Register your models here.
+admin.site.register(Booking)  # Replace ... with your model names to register them in the admin site.
--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
-# from listings.models import Listing
-# from users.models import User
+from django.db import models
+from listings.models import Listing
+from users.models import User
 
-# class Booking(models.Model):
-#     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     start_date = models.DateField()
-#     end_date = models.DateField()
-#     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-#     status = models.CharField(max_length=50, default="CONFIRMED")
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, default="CONFIRMED")
