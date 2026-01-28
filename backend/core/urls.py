163bdae5dff4319a3899988a6d317109cda11f61
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-28 16:57:15 +0530

    provider fix

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('providers', include('providers.urls')),
-    path('admins', include('admins.urls')),
-    path('payments', include('payments.urls')),
-    path('reviews', include('reviews.urls')),
+    path('admins', include('admins.urls')),   
     path('listings', include('listings.urls')),
-    # path('messages', include('messages.urls')),
     path("api/bookings/", include("bookings.urls")),
 ]
 
