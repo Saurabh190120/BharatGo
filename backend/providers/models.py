ddd8bb55e8a7c1ecf000b5a8d1116f116cecfe7c
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-28 16:47:23 +0530

    provider fix

--- backend/providers/models.py
+++ backend/providers/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from users.models import User
-# Create your models here.
+
 class Provider(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=150)
     contact_email = models.EmailField()
     contact_phone = models.CharField(max_length=20)
-    description = models.TextField()
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.company_name
