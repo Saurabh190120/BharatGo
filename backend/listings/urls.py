ee4518fdfc1e6f94705b7177e2f7effc0a8d7443
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-30 20:51:42 +0530

    provider fix

--- backend/listings/urls.py
+++ backend/listings/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import create_listing,create_bus_listing
+from .views import create_bus_listing
 
 urlpatterns = [
-    path("create/", create_listing),
+
     path("bus/create/", create_bus_listing),
 ]
