101f081e99e49b44c4b73a66f3e6f84192b0e6ff
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:37:57 +0530

    trial

--- bus_booking/bookings/views.py
+++ bus_booking/bookings/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Booking
+from buses.models import Seat, Schedule
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_booking(request):
+    schedule_id = request.data['schedule_id']
+    seats = request.data['seats']  # ["A1", "A2"]
+
+    schedule = Schedule.objects.get(id=schedule_id)
+
+    total_amount = len(seats) * schedule.price
+
+    for seat_no in seats:
+        seat = Seat.objects.get(schedule=schedule, seat_number=seat_no)
+        if seat.is_booked:
+            return Response({"error": "Seat already booked"}, status=400)
+        seat.is_booked = True
+        seat.save()
+
+    booking = Booking.objects.create(
+        user=request.user,
+        schedule=schedule,
+        seat_numbers=",".join(seats),
+        total_amount=total_amount,
+        status="CONFIRMED"
+    )
+
+    return Response({"message": "Booking successful", "booking_id": booking.id})
