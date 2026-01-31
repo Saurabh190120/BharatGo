7db6af32454ede92736abd67b543dd6ab8989190
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:23:12 +0530

    trial

--- bus_booking/bookings/models.py
+++ bus_booking/bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
+from buses.models import Schedule
 
-# Create your models here.
+class Booking(models.Model):
+    STATUS_CHOICES = (
+        ('CONFIRMED', 'Confirmed'),
+        ('CANCELLED', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    seat_numbers = models.CharField(max_length=50)
+    total_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    booked_at = models.DateTimeField(auto_now_add=True)
