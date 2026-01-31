aa73dbf5cd89758074d145416fee207a497a1c50
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:36:17 +0530

    trial

--- bus_booking/buses/views.py
+++ bus_booking/buses/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Schedule, Route
+from .models import Schedule, Route,Seat
 from .serializers import BusSearchSerializer
 
 @api_view(['GET'])
@@ -14,3 +14,10 @@ def search_buses(request):
 
     serializer = BusSearchSerializer(schedules, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def seat_availability(request, schedule_id):
+    seats = Seat.objects.filter(schedule_id=schedule_id)
+    data = [{"seat": s.seat_number, "is_booked": s.is_booked} for s in seats]
+    return Response(data)
