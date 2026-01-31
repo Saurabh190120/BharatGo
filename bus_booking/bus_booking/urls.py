48ee065e4560523d07d7dfbe051da2a5979e53d1
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:33:26 +0530

    trial

--- bus_booking/bus_booking/urls.py
+++ bus_booking/bus_booking/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
 ]
