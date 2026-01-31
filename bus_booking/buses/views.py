0c9ff178c902af0683e1f7d292373a2c21e6fe5c
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:34:43 +0530

    trial

--- bus_booking/buses/views.py
+++ bus_booking/buses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Schedule, Route
+from .serializers import BusSearchSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def search_buses(request):
+    source = request.GET.get('source')
+    destination = request.GET.get('destination')
+    date = request.GET.get('date')
+
+    routes = Route.objects.filter(source__iexact=source, destination__iexact=destination)
+    schedules = Schedule.objects.filter(route__in=routes, date=date)
+
+    serializer = BusSearchSerializer(schedules, many=True)
+    return Response(serializer.data)
