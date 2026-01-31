37d60c1d3b9031c98a7d520e444281a0ed401fd9
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:28:36 +0530

    trial

--- bus_booking/buses/admin.py
+++ bus_booking/buses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Bus, Route, Schedule, Seat
+admin.site.register(Bus)
+admin.site.register(Route)
+admin.site.register(Schedule)
+admin.site.register(Seat)
