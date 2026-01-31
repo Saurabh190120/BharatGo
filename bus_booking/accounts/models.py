eeb5c09884e4912efa33e4fa92b6bc7546b9b177
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-01-31 23:20:34 +0530

    trial

--- bus_booking/accounts/models.py
+++ bus_booking/accounts/models.py
@@ -8,3 +8,11 @@ class User(AbstractUser):
         ('ADMIN', 'Admin'),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='USER')
+
+class Provider(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.company_name
