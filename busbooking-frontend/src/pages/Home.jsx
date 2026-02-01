ee03a3da3aac4fc2c6d4cf459cb5344adb20c1a8
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-02-01 16:02:46 +0530

    trial

--- busbooking-frontend/src/pages/Home.jsx
+++ busbooking-frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
       <input className="border p-3 rounded" placeholder="From" />
       <input className="border p-3 rounded" placeholder="To" />
       <input type="date" className="border p-3 rounded" />
-      <button className="bg-red-500 text-white rounded text-lg">
+      <button className="bg-red-500 text-white rounded text-lg" onClick={search}>
         Search
       </button>
     </div>
