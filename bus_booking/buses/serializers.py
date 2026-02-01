65acfdd35b27703c3cc8f05f3c5e1c6df51b3ac8
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-02-01 16:45:04 +0530

    trial

--- bus_booking/buses/serializers.py
+++ bus_booking/buses/serializers.py
@@ -22,3 +22,8 @@ class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
         fields = '__all__'
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = '__all__'
