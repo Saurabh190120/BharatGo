728913897ef96219f746e00b4cbf018b0771ecf4
Saurabh190120 <saurabhgurjar1901@gmail.com>
2026-02-01 16:41:13 +0530

    trial

--- bus_booking/buses/urls.py
+++ bus_booking/buses/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import search_buses, seat_availability
+from .views import search_buses, seat_availability, add_bus
 
 urlpatterns = [
     path('search/', search_buses),
     path('seats/<int:schedule_id>/', seat_availability),
+    path('provider/bus/', add_bus),
+
 
 ]
